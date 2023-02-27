3c1788c9a2bbb3b56d5fa840cf702601757ec3d4
Álvaro <ainiguezdislaa@gmail.com>
2023-02-27 11:05:36 +0100

    ej terminado

--- src/aplicacion/Principal.java
+++ src/aplicacion/Principal.java
@@ -1,2 +1,13 @@
-package aplicacion;public class Principal {
+package aplicacion;
+import java.util.ArrayList;
+import java.util.List;
+import dominio.ej14_8;
+public class Principal {
+    public static void main(String[] args) {
+        List<String>lista = new ArrayList<>();
+        lista.add("Hola");
+        lista.add("Mundo");
+        lista.add("Java");
+        System.out.println(dominio.ej14_8.aMinusculas(lista));
+    }
 }
--- src/dominio/ej14_8.java
+++ src/dominio/ej14_8.java
@@ -1,2 +1,11 @@
-package dominio;public class ej14_8 {
+package dominio;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+public class ej14_8 {
+    public static List<String> aMinusculas(List<String> lista){
+        return Stream.iterate(0, i -> i + 1).limit(lista.size()).map(i -> lista.get(i).toLowerCase()).collect(Collectors.toList());
+    }
+
 }
